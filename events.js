17ce03d21c44f2b50fd88b32f494c0217b2eb975
AllainDM <78161319+AllainDM@users.noreply.github.com>
2021-06-01 09:34:43 +0300

    Тут были проценты

--- events.js
+++ events.js
@@ -1,80 +1,19 @@
 let output = document.getElementById('output');
-
-
-// Использование более сложных функций
-let squ = false;
-let exp = false;
+let result;
 
 // Последнее введенное число пользователем
 let num = true;
 let number = '';  
 let lastNumber = 1; 
+let lastLastNumber = 1;
 
-let mainOutput = '';    // Главная строка для преобразования
+let arrString = [];     //Массив строк  √
+let newArrString = [];
 
-let arrString = [];     //Массив строк
 
 document.getElementById('sum').addEventListener('click', () => {
-    if (squ) {
-        mainOutput += `)`;
-        let result = calc(mainOutput);
-        output.innerHTML += ` = ${result}`
-    } else {
-        let result = calc(mainOutput);
-        output.innerHTML += ` = ${result}`
-    }
-    console.log(mainOutput);
-    console.log('sum')
-    console.log(output.innerText);
-});
-
-document.getElementById('%').addEventListener('click', () => {
-    output.innerHTML += `%`;
-    mainOutput = mainOutput.slice(0, -number.length);
-    mainOutput += `${lastNumber}*${number}/100`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = '1';
-    number = '';
-    console.log(mainOutput);
-    console.log('%')
-});
-
-document.getElementById('exp').addEventListener('click', () => {
-    output.innerHTML += `²`;
-    // mainOutput = mainOutput.slice(0, -number.length);
-    // mainOutput += `Math.pow(${number}, 2)`;
-    mainOutput += '**2';
-    arrString.push(mainOutput);
-    console.log(arrString);
-    console.log(mainOutput);
-});
-
-document.getElementById('back').addEventListener('click', () => {
-    // mainOutput = mainOutput.slice(0, -1);
-    arrString.pop();
-    mainOutput = arrString[arrString.length - 1];
-    console.log(arrString);
-    output.innerHTML = output.innerHTML.slice(0, -1);
-    console.log(mainOutput);
-});
-
-document.getElementById('squ').addEventListener('click', () => {
-    output.innerHTML += `√`;
-    squ = true;
-    mainOutput += `Math.sqrt(`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    console.log(mainOutput);
-});
-
-document.getElementById('.').addEventListener('click', () => {
-    output.innerHTML += `.`;
-    mainOutput += `.`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    console.log(mainOutput);
-    console.log('.')
+    arrString = output.innerText.split('');
+    sum();
 });
 
 document.getElementById('clear').addEventListener('click', () => {
@@ -83,246 +22,101 @@ document.getElementById('clear').addEventListener('click', () => {
     arrString = [];
     console.log(arrString);
     number = '';
+    lastNumber = '';
+    lastLastNumber = '';
     console.log('clear')
+});
+
+document.getElementById('back').addEventListener('click', () => {
+    output.innerHTML = output.innerHTML.slice(0, -1);
+    console.log(output.innerHTML);
+});
+
+document.getElementById('%').addEventListener('click', () => {
+    output.innerHTML += `%`;
+    console.log('%')
+});
+
+document.getElementById('exp').addEventListener('click', () => {
+    output.innerHTML += `²`;
+});
+
+document.getElementById('squ').addEventListener('click', () => {
+    output.innerHTML += `√`;
+});
+
+document.getElementById('.').addEventListener('click', () => {
+    output.innerHTML += `.`;
+    console.log('.');
 });
 
 document.getElementById('(').addEventListener('click', () => {
     output.innerHTML += `(`;
-    mainOutput += `(`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
-    console.log(mainOutput);
     console.log('(')
 });
 
 document.getElementById(')').addEventListener('click', () => {
     output.innerHTML += `)`;
-    mainOutput += `)`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
-    console.log(mainOutput);
     console.log(')')
 });
 
 document.getElementById('+').addEventListener('click', () => {
-    if (squ) {
-        output.innerHTML += `+`;
-        mainOutput += `)+`;
-        squ = false;
-        console.log(mainOutput);
-        console.log('+')
-    } else {
-        output.innerHTML += `+`;
-        mainOutput += `+`;
-        console.log(mainOutput);
-        console.log('+')
-    }
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
+    output.innerHTML += `+`;
+    console.log('+')
 });
 
 document.getElementById('-').addEventListener('click', () => {
-    if (squ) {
-        output.innerHTML += `-`;
-        mainOutput += `)-`;
-        squ = false;
-        console.log(mainOutput);
-        console.log('-')
-    } else {
-        output.innerHTML += `-`;
-        mainOutput += `-`;
-        console.log(mainOutput);
-        console.log('-')
-    }
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
+    output.innerHTML += `-`;
+    console.log('-')
 });
 
 document.getElementById('/').addEventListener('click', () => {
     output.innerHTML += `/`;
-    mainOutput += `/`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('/')
 });
 
 document.getElementById('*').addEventListener('click', () => {
     output.innerHTML += `*`;
-    mainOutput += `*`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    lastNumber = number;
-    num = false;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('*')
 });
 
-const one = document.getElementById('1').addEventListener('click', () => {
-    if (num) {
-        number += `1`;
-    } else {
-        number = `1`;
-    }
-    mainOutput += `1`;
-    arrString.push(mainOutput);
-    console.log(arrString);
+document.getElementById('1').addEventListener('click', () => {
     output.innerHTML += `1`;
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('1')
 });
-one;
-
 document.getElementById('2').addEventListener('click', () => {
-    if (num) {
-        number += `2`;
-    } else {
-        number = `2`;
-    }
     output.innerHTML += `2`;
-    mainOutput += `2`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput);
     console.log('2');
 });
 document.getElementById('3').addEventListener('click', () => {
-    if (num) {
-        number += `3`;
-    } else {
-        number = `3`;
-    }
     output.innerHTML += `3`;
-    mainOutput += `3`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput);
     console.log('3');
 });
 document.getElementById('4').addEventListener('click', () => {
-    if (num) {
-        number += `4`;
-    } else {
-        number = `4`;
-    }
     output.innerHTML += `4`;
-    mainOutput += `4`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('4')
 });
 document.getElementById('5').addEventListener('click', () => {
-    if (num) {
-        number += `5`;
-    } else {
-        number = `5`;
-    }
     output.innerHTML += `5`;
-    mainOutput += `5`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('5')
 });
 document.getElementById('6').addEventListener('click', () => {
-    if (num) {
-        number += `6`;
-    } else {
-        number = `6`;
-    }
     output.innerHTML += `6`;
-    mainOutput += `6`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('6')
 });
 document.getElementById('7').addEventListener('click', () => {
-    if (num) {
-        number += `7`;
-    } else {
-        number = `7`;
-    }
     output.innerHTML += `7`;
-    mainOutput += `7`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('7')
 });
 document.getElementById('8').addEventListener('click', () => {
-    if (num) {
-        number += `8`;
-    } else {
-        number = `8`;
-    }
     output.innerHTML += `8`;
-    mainOutput += `8`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('8')
 });
 document.getElementById('9').addEventListener('click', () => {
-    if (num) {
-        number += `9`;
-    } else {
-        number = `9`;
-    }
     output.innerHTML += `9`;
-    mainOutput +=  `9`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('9')
 });
 document.getElementById('0').addEventListener('click', () => {
-    if (num) {
-        number += `0`;
-    } else {
-        number = `0`;
-    }
     output.innerHTML += `0`;
-    mainOutput += `0`;
-    arrString.push(mainOutput);
-    console.log(arrString);
-    num = true;
-    console.log(`number = ${number}`);
-    console.log(mainOutput)
     console.log('0')
 });
--- script.js
+++ script.js
@@ -1,6 +1,64 @@
+function sum() {
+    
+    // arr();
+    for (i = 0; i < arrString.length; i++) {
+        if (!isNaN(arrString[i])) {
+            number += arrString[i];
+            console.log(`Число: ${number}`);
+            // console.log(`тут число: ${arrString[i]}`)
+        }
+        if (isNaN(arrString[i])) {
+            lastLastNumber = lastNumber;
+            lastNumber = number;
+            number = '';
+            console.log(`Самое последнее число: ${lastLastNumber}`);
+            console.log(`Последнее число: ${lastNumber}`);
+        }
+        if (arrString[i] == '%') {
+            arrString[i - lastNumber.length] = `(${arrString[i - lastNumber.length]}`
+            delete arrString[i];
+            for (n = i + 1; n <= arrString.length; n++) {
+                if (isNaN(arrString[n])) {
+                    console.log(`index: ${n}`);
+                    if (arrString[n] == undefined) {
+                        arrString[n] = `*${lastLastNumber}/100)`;
+                    } else {
+                        arrString[n] = `*${lastLastNumber}/100)` + arrString[n];
+                    }
+                    console.log(n);
+                    break;
+                }
+            }
+        } 
+        if (arrString[i] == '²') {
+            arrString[i] = '**2';
+        } 
+        if (arrString[i] == '√') {
+            delete arrString[i];
+            for (n = i + 1; n <= arrString.length; n++) {
+                if (isNaN(arrString[n])) {
+                    console.log(`index: ${n}`);
+                    if (arrString[n] == undefined) {
+                        arrString[n] = '**0.5';
+                    } else {
+                        arrString[n] = '**0.5' + arrString[n];
+                    }
+                    console.log(n);
+                    break;
+                }
+            }
+        } 
+    }
+
+    let x = arrString.join('');
+    result = calc(x);
+    
+    output.innerHTML += ` = ${result}`
+
+}
+
+module.exports = sum;
+
 function calc(str) {
     return (new Function('return ' + str))();
 }
-
-module.exports = calc;
-
