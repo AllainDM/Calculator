8206f783e26f3f157a072fe95d8f9e4189c1f036
AllainDM <78161319+AllainDM@users.noreply.github.com>
2021-06-07 17:43:25 +0300

    First finished version

--- script.js
+++ script.js
@@ -1,64 +1,36 @@
-function sum() {
-    
-    // arr();
-    for (i = 0; i < arrString.length; i++) {
-        if (!isNaN(arrString[i])) {
-            number += arrString[i];
-            console.log(`Число: ${number}`);
-            // console.log(`тут число: ${arrString[i]}`)
-        }
-        if (isNaN(arrString[i])) {
-            lastLastNumber = lastNumber;
-            lastNumber = number;
-            number = '';
-            console.log(`Самое последнее число: ${lastLastNumber}`);
-            console.log(`Последнее число: ${lastNumber}`);
-        }
-        if (arrString[i] == '%') {
-            arrString[i - lastNumber.length] = `(${arrString[i - lastNumber.length]}`
-            delete arrString[i];
-            for (n = i + 1; n <= arrString.length; n++) {
-                if (isNaN(arrString[n])) {
-                    console.log(`index: ${n}`);
-                    if (arrString[n] == undefined) {
-                        arrString[n] = `*${lastLastNumber}/100)`;
-                    } else {
-                        arrString[n] = `*${lastLastNumber}/100)` + arrString[n];
-                    }
-                    console.log(n);
-                    break;
-                }
-            }
+
+function sum(str) {
+    str = str.split('');
+    for (i = 0; i < str.length; i++) {
+        if (str[i] == '²') {
+            str[i] = '**2';
         } 
-        if (arrString[i] == '²') {
-            arrString[i] = '**2';
-        } 
-        if (arrString[i] == '√') {
-            delete arrString[i];
-            for (n = i + 1; n <= arrString.length; n++) {
-                if (isNaN(arrString[n])) {
-                    console.log(`index: ${n}`);
-                    if (arrString[n] == undefined) {
-                        arrString[n] = '**0.5';
+        if (str[i] == '√') {
+            delete str[i];
+            for (n = i + 1; n <= str.length; n++) {
+                if (isNaN(str[n])) {
+                    // console.log(`index: ${n}`);
+                    if (str[n] == undefined) {
+                        str[n] = '**0.5';
                     } else {
-                        arrString[n] = '**0.5' + arrString[n];
+                        str[n] = '**0.5' + str[n];
                     }
-                    console.log(n);
+                    // console.log(n);
                     break;
                 }
             }
         } 
     }
 
-    let x = arrString.join('');
-    result = calc(x);
-    
-    output.innerHTML += ` = ${result}`
+    str = str.join('');
+    str = calc(str);
+    return str;
 
 }
-
-module.exports = sum;
 
 function calc(str) {
     return (new Function('return ' + str))();
 }
+
+module.exports = sum;
+
--- script.test.js
+++ script.test.js
@@ -3,40 +3,43 @@ const sum = require('./script');
 
 test('text 1', () => {
     expect(sum("5+4")).toBe(9);
+    expect(sum("30.5+16.8*(2+3)")).toBe(114.5);
+
 
 });
 
 test('text 2', () => {
-    expect(sum("√36+14-7*(21+4)")).toBe(-155);
-
-});
-
-test('text 3', () => {
-    expect(sum("(12+4)*15+4²")).toBe(246);
-
-});
-
-test('text 4', () => {
-    expect(sum("30+15%-4²+2*3")).toBe(24.5);
-
-});
-
-test('text 5', () => {
-    expect(sum("9/3+6²-√81")).toBe(30);
-
-});
-
-test('text 6', () => {
     expect(sum("30.5+16.8*(2+3)")).toBe(114.5);
 
 });
 
-test('text 7', () => {
+test('text 3', () => {
     expect(sum("0.2+0.2")).toBe(0.4);
 
 });
 
-test('text 8', () => {
+test('text 4', () => {
+    expect(sum("√36+14-7*(21+4)")).toBe(-155);
+
+});
+
+test('text 5', () => {
+    expect(sum("(12+4)*15+4²")).toBe(256);
+
+});
+
+// test('text -', () => {
+//     expect(sum("30+15%-4²+2*3")).toBe("30+15%-4²+2*3");
+
+// });
+
+test('text 6', () => {
+    expect(sum("9/3+6²-√81")).toBe(30);
+
+});
+
+
+test('text 7', () => {
     expect(sum("0.5*12*(3+2²)")).toBe(42);
 
 });
